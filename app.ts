8c53c8c32b90f21c31b26071a2b14e4cbc2b57ad
Bara Afanah <baraafanah1@gmail.com>
2026-08-23 19:37:59 +0300

    CORS config

--- app.ts
+++ app.ts
@@ -5,9 +5,20 @@ import articleRoutes from "./routes/articleRoutes";
 import { generalLimiter } from "./middleware/rateLimiters";
 import helmet from "helmet";
 import AppError from "./utils/AppError";
+import cors from "cors";
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") ?? [];
 
 const app = express();
 app.use(helmet());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(generalLimiter);
 app.use(express.json({ limit: "10kb" }));
 app.use("/auth", authRoutes);
